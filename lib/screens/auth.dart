2873753a9a3c4e704bceb90c66588276229e23e8
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-02-27 13:26:19 +0700

    Validating User Input

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,7 +8,20 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
   var _isLogin = false;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+    }
+    print(_enteredEmail);
+    print(_enteredPassword);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,43 +39,65 @@ class _AuthScreenState extends State<AuthScreen> {
               child: Padding(
                 padding: EdgeInsets.all(16),
                 child: Form(
+                    key: _form,
                     child: Column(children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Email Address',
-                    ),
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    textCapitalization: TextCapitalization.none,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                    ),
-                    autocorrect: false,
-                    keyboardType: TextInputType.visiblePassword,
-                    textCapitalization: TextCapitalization.none,
-                    obscureText: true,
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primaryContainer),
-                      onPressed: () {},
-                      child: _isLogin ? Text('Login') : Text('Signup')),
-                  TextButton(
-                      onPressed: () {
-                        setState(() {
-                          _isLogin = !_isLogin;
-                        });
-                      },
-                      child: _isLogin
-                          ? Text('Create an account')
-                          : Text('I already have an account'))
-                ])),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Email Address',
+                        ),
+                        autocorrect: false,
+                        keyboardType: TextInputType.emailAddress,
+                        textCapitalization: TextCapitalization.none,
+                        validator: (value) {
+                          if (value == null ||
+                              value.trim().isEmpty ||
+                              !value.contains('@')) {
+                            return 'Please input a valid email address';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _enteredEmail = value!;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null ||
+                              value.trim().isEmpty ||
+                              value.length > 6) {
+                            return 'Password must be at least 6 characters long';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _enteredPassword = value!;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer),
+                          onPressed: _submit,
+                          child: _isLogin ? Text('Login') : Text('Signup')),
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: _isLogin
+                              ? Text('Create an account')
+                              : Text('I already have an account'))
+                    ])),
               ),
             )
           ]),
