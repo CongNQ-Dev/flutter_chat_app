fd4c3035585f96ffd94569528cfcc43f70afec39
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-02-28 18:13:43 +0700

    Adding ChatMessages & Input Widgets

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +22,9 @@ class ChatScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: Text('Logged in!'),
+      body: Column(
+        children: const [Expanded(child: ChatMessage()), NewMessage()],
+      ),
     );
   }
 }
